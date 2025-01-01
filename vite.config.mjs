4c6ea12d75f1bf9e34a5628126a7ab1714fd0636
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2025-01-01 10:27:49 -0800

    get rid of frontend and backend directories

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  root: "./frontend",
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
