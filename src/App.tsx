22da834d6dda829653cb5e812b5b3c18063a37e0
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2025-01-02 12:01:55 -0800

    remove emotion refs

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 
 import { MantineProvider } from "@mantine/core";
-import { emotionTransform, MantineEmotionProvider } from "@mantine/emotion";
 import { Router } from "./Router";
 import { theme } from "./theme";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme="dark" stylesTransform={emotionTransform}>
-      <MantineEmotionProvider>
-        <Router />
-      </MantineEmotionProvider>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <Router />
     </MantineProvider>
   );
 }
